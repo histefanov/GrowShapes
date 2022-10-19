d4e041fde213e7d0be380ac17636cbd35b5d3522
Simeon Stefanov <simeon.stefanov@accedia.com>
2022-10-19 17:28:28 +0300

    square class added

---
+++ GrowShapes/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowShapes
+{
+    internal class Square : Shape
+    {
+        private double _side;
+
+        public Square(double a)
+        {
+            this._side = a;
+        }
+
+        public override double GetArea() => _side * _side;
+        public override double GetPerimeter() => 4 * _side;
+    }
+}
