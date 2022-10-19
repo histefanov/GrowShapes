3f16d0bd40e1d350af7d0f88d520851ecd131b6e
Simeon Stefanov <simeon.stefanov@accedia.com>
2022-10-19 17:32:18 +0300

    added circle class

---
+++ GrowShapes/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowShapes
+{
+    internal class Circle : Shape
+    {
+        private double _radius;
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea() => Math.PI * Math.Pow(_radius, 2);
+        public override double GetPerimeter() => 2 * Math.PI * _radius;
+    }
+}
