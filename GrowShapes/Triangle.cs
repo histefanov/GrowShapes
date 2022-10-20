c12220c79ad1612c6807db743ba7f54ab5a871c5
Simeon Stefanov <simeon.stefanov@accedia.com>
2022-10-20 14:13:26 +0300

    triangle class implemented. getarea func.

---
+++ GrowShapes/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowShapes
+{
+    internal class Triangle : Shape
+    {
+        private double _aside;
+        private double _bside;
+        private double _cside;
+
+        public Triangle(double a, double b, double c)
+        {
+            this._aside = a;
+            this._bside = b;
+            this._cside = c;
+        }
+
+        public override double GetArea()
+        {
+            double semiPerimeter = (_aside + _bside + _cside) / 2;
+            return Math.Sqrt(semiPerimeter *
+                            (semiPerimeter - _aside) * (semiPerimeter - _bside) * (semiPerimeter - _cside));
+        }
+
+        // TODO IMPLEMENT
+        public override double GetPerimeter() => -1;
+    }
+}
